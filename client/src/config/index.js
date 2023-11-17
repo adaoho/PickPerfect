969e702196b59b1cae47e5c5fb3cd2738eef8c8a
adaoho <9nugroho@gmail.com>
2023-11-17 13:36:35 +0700

    updated deploy

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const PICKPERFECT_API = "http://server.adaoho-project.my.id";
-const PICKPERFECT_API = "http://localhost:3000";
+const PICKPERFECT_API = "http://server.adaoho-project.my.id";
 const pickPerfectApi = axios.create({
   baseURL: PICKPERFECT_API,
 });
 
 export default pickPerfectApi;
+// const PICKPERFECT_API = "http://localhost:3000";
